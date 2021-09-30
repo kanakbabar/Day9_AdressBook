523544261bf6138b26779ee6b05f4ae7d488f5c9
kanakbabar <kanakbabar999@gamil.com>
2021-09-30 18:59:48 +0530

    [kanak]To creat contact in adress book

---
+++ program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9AdressBook
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            //uc1_CreatContact.CC();
+            // uc2_Add.AddNewContact();
+            //uc3_Edit.Edit();
+            uc4_Delete.Delete();
+            Console.ReadLine();
+
+
+        }
+          
+    }
+}
---
+++ uc1_CreatContact.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9AdressBook
+{
+    class uc1_CreatContact
+
+    {
+        
+       
+       public static void CC()
+       {
+            Console.WriteLine("start creat contact in adress book ...");
+            Console.WriteLine("Enter your first name :");
+             Console.WriteLine(Console.ReadLine());
+            Console.WriteLine("Enter your last name :");
+            Console.WriteLine(Console.ReadLine());
+            Console.WriteLine("Enter your address :");
+            Console.WriteLine(Console.ReadLine());
+            Console.WriteLine("Enter your city:");
+            Console.WriteLine(Console.ReadLine());
+            Console.WriteLine("Enter your state :");
+            Console.WriteLine(Console.ReadLine());
+            Console.WriteLine("Enter zip :");
+            Console.WriteLine(Console.ReadLine());
+            Console.WriteLine("Enter your phone number :");
+            Console.WriteLine(Console.ReadLine());
+            Console.WriteLine("Enter your Email :");
+            Console.WriteLine(Console.ReadLine());
+            Console.ReadLine();
+        
+       
+       }
+    }
+}
