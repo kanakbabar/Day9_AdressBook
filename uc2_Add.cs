826afe75cf899553dbfe57a6704cd92c687033ff
kanakbabar <kanakbabar999@gamil.com>
2021-09-30 19:01:52 +0530

    [kanak]To add new contact

---
+++ uc2_Add.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9AdressBook
+{
+    class uc2_Add
+    {
+        string NewContact;
+       public static void AddNewContact()
+       {
+            Console.WriteLine("Add new contact in adress book ....");
+            Console.WriteLine("Enter new contact :");
+            Console.WriteLine(Console.ReadLine());
+            Console.ReadLine();
+       }
+       
+        
+    }
+}
